719996d165dabff02feb81d0aed45a0a25306f5c
MineMaster385 <alexa24683931@gmail.com>
2020-04-11 23:48:46 +0200

    amélioration des commande + ajout de la commande sinfo + réparation du message d'aurevoir

--- main.js
+++ main.js
@@ -14,28 +14,40 @@ client.on("error", console.error);
 client.on("warn", console.warn);
 // client.on("debug", console.log);
 
+// ping
 client.on("message", msg => {
-  if (msg.author.bot) return;
   const args = msg.content.split(/ +/g);
   const cmd = args.shift().toLowerCase();
+  if (msg.author.bot) return;
   if (cmd === "ping") msg.channel.send("Pong!");
-  if (cmd === `${PREFIX}say`) {
+});
+
+client.on("message", msg => {
+  if (msg.author.bot) return;
+  if (msg.content.indexOf(PREFIX) !== 0) return;
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd === "say") {
     if (!msg.member.hasPermission("MANAGE_MESSAGES")) return "Tu n'a pas les permission requise!";
     msg.delete();
     msg.channel.send(args.join(" "));
   }
-  /* if (cmd === `${PREFIX}role`) {
-    const role = msg.guild.roles.cache.find(r => r.name === args[0]);
-    if (!msg.member.hasPermission("MANAGE_ROLES")) return "Tu n'a pas les permission requise!";
-    if (!role) return "Ce rôle n'existe pas";
-    if (msg.member.roles.cache.find(r => r.name === args[0])) {
-      msg.member.roles.remove(role);
-      msg.channel.send(`J'ai suprimé le rôle ${role} à ${msg.author}`);
-    } else {
-      msg.member.roles.add(role);
-      msg.channel.send(`J'ai ajouté le rôle ${role} à ${msg.author}`);
-    }
-  }*/
+  if (cmd === "sinfo") {
+    msg.delete();
+    const embed = new Discord.MessageEmbed() // on crée l'embed
+      .setColor("#FF0000")// couleur de l'embed
+      .setTitle(`__${msg.guild.name}__`)// titre ---- on met '\' devant le ' pour qu'il ne le compte aps en apostrophe fermante
+      .setDescription("Voici quelque information sur ce serveur")
+      .setThumbnail(msg.guild.iconURL)
+      .addField("**Propriétaire du serveur**", `${msg.guild.owner}`, true)
+      .addField("**ID du serveur**", msg.guild.id, true)
+      .addField("**Région du serveur**", msg.guild.region, true)
+      .addField("**Date de création du serveur**", msg.guild.createdAt, true)
+      .addField("**Nombre de membre**", `${msg.guild.memberCount}`, true)
+      .setTimestamp()
+      .setFooter(`Demandé par ${msg.author.username}`, `${msg.author.displayAvatarURL()}`); // ajoute un footer
+    msg.channel.send(embed);
+  }
 });
 
 // message de bienvenue
