99a7db6253c2f556bb2e8de65750e7c9bcce91eb
MineMaster385 <alexa24683931@gmail.com>
2020-04-11 18:22:32 +0200

    ajout du statut

---
+++ main.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableEveryone: true });
+
+client.login('NTg5NDczODY2NjMyMzk2ODIx.XpG8Ww.DOUdw5UnadGggadNUBghZNSioJY');
+
+client.on('ready', () => {
+    console.log(` `);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity(`%help ┊ ${client.guilds.cache.size} serveurs ┊ ${client.users.cache.size} utilisateurs`, {type: 'WATCHING'})
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') msg.channel.send('Pong!');
+  if (msg.content === 'voila') msg.channel.send('@everyone');
+  
+});
+
